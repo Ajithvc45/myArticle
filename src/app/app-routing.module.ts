f0f02d5ab9e48d5fbca1d5bcaec6f3e5902013d7
ajith82 <ajith@sketchbrahma.com>
2023-01-31 11:06:42 +0530

    2 comp created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AddNewComponent } from './add-new/add-new.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'add',component:AddNewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
